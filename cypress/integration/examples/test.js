3117fcaffff07e1fc7fffa7b8e6af59d8812f143
patis <arm1patis@gmail.com>
2021-03-09 15:32:29 +0700

    Lap

---
+++ cypress/integration/examples/test.js
@@ -0,0 +1,9 @@
+describe('Test', () => {
+    it('Visits the Kitchen Sink', () => {
+      cy.visit('https://example.cypress.io')
+      cy.contains('type').click()
+      cy.get('.action-email')
+        .type('fake@email.com')
+    })
+  })  
+   
